9292e50d99b419f491dba0579e971696f1e8a22c
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2017-10-18 14:42:02 +0200

    Example HACL: run a loop to get diagnostics in shell

--- examples/hacl/main.c
+++ examples/hacl/main.c
@@ -27,5 +27,9 @@ int main(void)
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
+    while(1) {
+      printf("HACL* is running !\n");
+    }
+
     return 0;
 }
